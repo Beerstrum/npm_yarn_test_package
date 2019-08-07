65b92dc4970d6c02e31db7eb57398703f225dd06
Adam Bjurstrom <adam.bjurstrom@hexagon.com>
2019-08-07 14:12:20 -0500

    Removed printMessage. Added printSendoff.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 /**
- * This is a test function that sends a message to the console.
- */
-exports.printMessage = function() {
-  console.log('This is a message from the demo-test-package version 2+');
-};
-
-/**
  * This function greets the user by name.
  * @param name who to say hello to
  */
 exports.printGreeting = function(name) {
   console.log(`Hello there ${name}!`);
+};
+
+/**
+ * This function says goodbye to the user by name.
+ * @param name who to say goodbye to
+ */
+exports.printSendoff = function(name) {
+  console.log(`Goodbye, ${name}`);
 };
