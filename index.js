b89f37d198340b6028850114e877190d6b019630
Adam Bjurstrom <adam.bjurstrom@hexagon.com>
2019-08-07 13:48:19 -0500

    Added new message

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/**
+ * This is a test function that sends a message to the console.
+ */
 exports.printMsg = function() {
   console.log('This is a message from the demo-test-package version 1.0.0');
 };
