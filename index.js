d94899bd824fadf4c060ba6b5611f290c2b9adf7
Adam Bjurstrom <adam.bjurstrom@hexagon.com>
2019-08-07 13:47:19 -0500

    v1.0.0

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 exports.printMsg = function() {
-  console.log('This is a message from the demo-test-package version 0.0.3');
+  console.log('This is a message from the demo-test-package version 1.0.0');
 };
