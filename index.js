92ec8a4ee48af64b2c7df8c75af9b5a514cb6fa3
Adam Bjurstrom <adam.bjurstrom@hexagon.com>
2019-08-07 13:55:34 -0500

    Added print greeting

--- index.js
+++ index.js
@@ -4,3 +4,11 @@
 exports.printMsg = function() {
   console.log('This is a message from the demo-test-package version 1.0.0');
 };
+
+/**
+ * This function greets the user by name.
+ * @param name who to say hello to
+ */
+exports.printGreeting = function(name) {
+  console.log(`Hello there ${name}`);
+};
