174ed0b54f385bd870787c7157b19919a56fcacd
Adam Bjurstrom <adam.bjurstrom@hexagon.com>
2019-08-07 14:04:54 -0500

    Fixed bug where old version was in message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * This is a test function that sends a message to the console.
  */
 exports.printMessage = function() {
-  console.log('This is a message from the demo-test-package version 1.0.0');
+  console.log('This is a message from the demo-test-package version 2+');
 };
 
 /**
